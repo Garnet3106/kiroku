a7656a47f46f58fce82a2b18074cb03a98e3f4b4
Garnet3106 <private@garnet.works>
2023-11-15 15:24:56 +0900

    Add app logo and slogan

--- src/components/Initialization/InitializationPage/Top.tsx
+++ src/components/Initialization/InitializationPage/Top.tsx
@@ -4,10 +4,18 @@ import Redux from '../../../redux/redux';
 import { InitializationPageIndex } from '../../../navigation';
 import InitializationPage from './InitializationPage';
 import { navigationActions } from '../../../redux/slices/navigation';
+import { StyleSheet, Text, View } from 'react-native';
+import AppLogo from '../../AppLogo';
 
 export default function Top() {
   return (
     <InitializationPage pageIndex={InitializationPageIndex.Top}>
+      <View style={styles.top}>
+        <Text style={styles.topSlogan}>
+        キャッチコピーが入ります
+        </Text>
+        <AppLogo height={45} />
+      </View>
       <RectangleButton
         text='初めての方はこちら'
         onPress={() => {
@@ -23,3 +31,20 @@ export default function Top() {
     </InitializationPage>
   );
 }
+
+const styles = StyleSheet.create({
+  top: {
+    display: 'flex',
+    position: 'absolute',
+    left: Ui.dimension.margin * 2,
+    top: '20%',
+    width: '100%',
+  },
+  topSlogan: {
+    color: Ui.color.main,
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+});

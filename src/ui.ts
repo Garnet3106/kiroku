481d02246ee72d121580149946fcf952d6b4d435
Garnet3106 <private@garnet.works>
2023-11-15 15:01:56 +0900

    Fix main color

--- src/ui.ts
+++ src/ui.ts
@@ -3,7 +3,7 @@ import { StyleProp } from 'react-native';
 namespace Ui {
   export const color = {
     background: '#eeeeee',
-    main: '#02c94b',
+    main: '#00ab69',
     white: '#ffffff',
     black: '#333333',
     gray: '#999999',
@@ -11,7 +11,7 @@ namespace Ui {
       lightGray: '#cccccc',
     },
     pressed: {
-      main: '#16db5e',
+      main: '#3db56f',
     },
   };
 

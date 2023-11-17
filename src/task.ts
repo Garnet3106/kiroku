edcb60e8ebe5a70cc5a6579d21544e299ff75c29
Garnet3106 <private@garnet.works>
2023-11-17 13:12:41 +0900

    Add task categories

--- src/task.ts
+++ src/task.ts
@@ -1,3 +1,28 @@
+export enum TaskCategory {
+  Uncategorized,
+  Reading,
+  Study,
+}
+
+export namespace TaskCategory {
+  export function enumerate(): TaskCategory[] {
+    return Object.values(TaskCategory).filter((v) => typeof v === 'number').map((v) => Number(v));
+  }
+
+  export function translate(category: TaskCategory): string {
+    switch (category) {
+      case TaskCategory.Uncategorized:
+      return '未分類';
+
+      case TaskCategory.Reading:
+      return '読書';
+
+      case TaskCategory.Study:
+      return '勉強';
+    }
+  }
+}
+
 export enum TaskSortStyle {
   Name,
   WorkingDay,

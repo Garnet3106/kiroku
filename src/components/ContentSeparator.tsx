e980a84f56eb37d349756cf4b93418ce43264c01
Garnet3106 <private@garnet.works>
2023-11-19 18:10:18 +0900

    Add style prop to content separator

--- src/components/ContentSeparator.tsx
+++ src/components/ContentSeparator.tsx
@@ -1,17 +1,21 @@
-import { ColorValue, View } from 'react-native';
+import { ColorValue, StyleProp, View, ViewStyle } from 'react-native';
 import Ui from '../ui';
 
 export type ContentSeparatorProps = Ui.LayoutProps & {
   color?: ColorValue,
+  style?: StyleProp<ViewStyle>,
 };
 
 export default function ContentSeparator(props: ContentSeparatorProps) {
   return (
-    <View style={{
-      backgroundColor: props.color ?? Ui.color.gray,
-      height: Ui.dimension.border.width,
-      marginBottom: props.insertBottomMargin ? Ui.dimension.margin : undefined,
-      width: '100%',
-    }} />
+    <View style={[
+      {
+        backgroundColor: props.color ?? Ui.color.gray,
+        height: Ui.dimension.border.width,
+        marginBottom: props.insertBottomMargin ? Ui.dimension.margin : undefined,
+        width: '100%',
+      },
+      props.style,
+    ]} />
   );
 }

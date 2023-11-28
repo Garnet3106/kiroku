0844b12c5a750178a2c728c38c3d8697c36c9efe
Garnet3106 <private@garnet.works>
2023-11-28 23:29:52 +0900

    Add async storage

---
+++ src/storage.ts
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export enum StorageKey {
+  EmailForLinkAuth = 'email-for-link-auth',
+}
+
+export namespace Storage {
+  export function getItem(key: StorageKey): Promise<string | null> {
+    return AsyncStorage.getItem(key);
+  }
+
+  export function setItem(key: StorageKey, value: string): Promise<void> {
+    return AsyncStorage.setItem(key, value);
+  }
+
+  export function removeItem(key: StorageKey): Promise<void> {
+    return AsyncStorage.removeItem(key);
+  }
+}

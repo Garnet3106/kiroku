b2e160515a6dfd84d3279792ea254d1121d8efee
Garnet3106 <private@garnet.works>
2023-12-04 23:41:49 +0900

    Fix status bar style

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Provider } from 'react-redux';
 import Redux from './src/redux/redux';
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import AppRoot from './src/components/AppRoot';
 import { RootSiblingParent } from 'react-native-root-siblings';
+import Ui from './src/ui';
 
 export default function App() {
   return (
     <Provider store={Redux.store}>
-      <StatusBar style={Platform.OS === 'android' ? 'dark' : 'dark'} />
+      <StatusBar style='dark' backgroundColor={Ui.color.white} />
       <SafeAreaProvider>
         {/* Specify flexGrow not to prevent scroll in ScrollView. */}
         <SafeAreaView style={{ flexGrow: 1 }}>
